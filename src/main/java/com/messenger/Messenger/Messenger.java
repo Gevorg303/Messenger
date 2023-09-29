c6cf35908aec50fdb0526226a5d122aed461b46d
Gevorg <gevorg_rabota@mail.ru>
2023-09-29 18:12:15 +0300

    Вроде все

--- src/main/java/com/messenger/Messenger/Messenger.java
+++ src/main/java/com/messenger/Messenger/Messenger.java
@@ -1,10 +1,10 @@
 package com.messenger.Messenger;
 
 import com.messenger.Messenger.domain.*;
-import com.messenger.Messenger.domain.ChatList;
-import com.messenger.Messenger.service.AdminServiceImpl;
 import com.messenger.Messenger.service.ChatServiceImpl;
-import com.messenger.Messenger.service.UserServiceImpl;
+import com.messenger.Messenger.service.impl.AdminServiceInterface;
+import com.messenger.Messenger.service.impl.ChatServiceInterface;
+import com.messenger.Messenger.service.impl.UserServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -12,11 +12,11 @@ import java.util.List;
 @Component
 public class Messenger {
     @Autowired
-    private UserServiceImpl userService;
+    private UserServiceInterface userServiceInterface;
     @Autowired
-    private ChatServiceImpl chatService;
+    private ChatServiceInterface chatServiceInterface;
     @Autowired
-    private AdminServiceImpl adminService;
+    private AdminServiceInterface adminServiceInterface;
     @Autowired
     private UserList userList;
     @Autowired
@@ -37,73 +37,73 @@ public class Messenger {
     public void setUserList(UserList userList) {
         this.userList = userList;
     }
-
-    public UserServiceImpl getUserService() {
-        return userService;
-    }
-
-    public ChatServiceImpl getChatService() {
-        return chatService;
+    @Autowired
+    public void setUserServiceInterface(UserServiceInterface userServiceInterface) {
+        this.userServiceInterface = userServiceInterface;
     }
     @Autowired
-    public void setChatService(ChatServiceImpl chatService) {
-        this.chatService = chatService;
+    public void setChatServiceInterface(ChatServiceInterface chatServiceInterface) {
+        this.chatServiceInterface = chatServiceInterface;
+    }
+    @Autowired
+    public void setAdminServiceInterface(AdminServiceInterface adminServiceInterface) {
+        this.adminServiceInterface = adminServiceInterface;
     }
 
-    public AdminServiceImpl getAdminService() {
-        return adminService;
+    public UserServiceInterface getUserServiceInterface() {
+        return userServiceInterface;
     }
-    @Autowired
-    public void setUserService(UserServiceImpl userService) {
-        this.userService = userService;
+
+    public ChatServiceInterface getChatServiceInterface() {
+        return chatServiceInterface;
     }
-    @Autowired
-    public void setAdminService(AdminServiceImpl adminService) {
-        this.adminService = adminService;
+
+    public AdminServiceInterface getAdminServiceInterface() {
+        return adminServiceInterface;
     }
 
     public User doNewUser(String userName) {
-        return getUserService().createUser(getUserList(), userName);
+        return getUserServiceInterface().createUser(getUserList(), userName);
     }
 
     public Chat doNewChat(User user1, String nameChat, boolean isPrivate, String password, int maxUser) {
-        return getUserService().createChat(getChatService(), getChatList(), user1, nameChat, isPrivate, password, maxUser);
+        return getUserServiceInterface().createChat((ChatServiceImpl) getChatServiceInterface(), getChatList(), user1, nameChat, isPrivate, password, maxUser);
     }
 
     public List<Chat> printListChat(User user) {
-        return getUserService().getUserChats(user);
+        return getUserServiceInterface().getUserChats(user);
     }
 
     public void removeChat(User user1, Chat chat1) {
-        getUserService().deleteChat(getChatService(), getChatList(), user1, chat1);
+        getUserServiceInterface().deleteChat((ChatServiceImpl) getChatServiceInterface(), getChatList(), user1, chat1);
     }
 
     public void changeChatMaxUsers(Admin admin, Chat chat1, int i) {
-        getAdminService().changeChatMaxUsers(admin, chat1, i);
+        getAdminServiceInterface().changeChatMaxUsers(admin, chat1, i);
     }
 
     public void addUserToChat(User user2, Chat chat, String password) {
-        getChatService().addUserToChat(user2, chat, password);
+        getChatServiceInterface().addUserToChat(user2, chat, password);
     }
 
     public void writeMessage(User user, Chat chat, Message message) {
-        getChatService().writeMessage(user,chat, message);
+        getChatServiceInterface().writeMessage(user,chat, message);
     }
 
     public void removeChatAdmin(Admin admin, Chat chat1) {
-        getAdminService().deleteAnyChat(getChatService(), getChatList(), admin, chat1);
+        getAdminServiceInterface().deleteAnyChat((ChatServiceImpl) getChatServiceInterface(), getChatList(), admin, chat1);
     }
 
     public Admin doNewAdmin(String nameAdmin) {
-        return getAdminService().crateAdmin(getUserList(), nameAdmin);
+        return getAdminServiceInterface().crateAdmin(getUserList(), nameAdmin);
     }
 
     public void removeUser(UserList userList, User user2) {
-        getUserService().deleteUserAndChats(getChatService(), getChatList(), userList, user2);
+        getUserServiceInterface().deleteUserAndChats((ChatServiceImpl) getChatServiceInterface(), getChatList(), userList, user2);
     }
 
     public void removeAdmin(Admin admin) {
-        getAdminService().deleteAdmin(getUserList(), admin);
+        getAdminServiceInterface().deleteAdmin(getUserList(), admin);
     }
 
     public List<Chat> printAllChat() {
